16a4615a713affb6e9ce8956c04e3d3a8c66ddf0
Vaishnavi Soni <vaishnavisoni72@gmail.com>
2024-10-08 12:34:31 +0530

    Create React app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+   
+    <div>
+      <h1>hello</h1>
+    </div>
+
+  )
+}
+
+export default App
