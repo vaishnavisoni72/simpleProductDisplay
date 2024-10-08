4857f09f3c05f2fbe8a4d3f42e724f89e1f6e1d6
Vaishnavi Soni <vaishnavisoni72@gmail.com>
2024-10-08 12:44:00 +0530

    Create Navigation

---
+++ src/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+export default function Navigation() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
